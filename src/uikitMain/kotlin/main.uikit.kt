e2bcf636fe528d74b6152bea51cef83076b8eb2d
Alex <bob298@yandex.ru>
2023-01-21 03:37:35 +0700

    ios setup

--- src/uikitMain/kotlin/main.uikit.kt
+++ src/uikitMain/kotlin/main.uikit.kt
@@ -1,6 +1,9 @@
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.material.Text
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.collectAsState
@@ -8,6 +11,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Application
 import data.features.settings.LocalSettingsEventBus
 import data.features.settings.SettingsEventBus
@@ -24,10 +28,14 @@ import platform.UIKit.UIApplication
 import platform.UIKit.UIApplicationDelegateProtocol
 import platform.UIKit.UIApplicationDelegateProtocolMeta
 import platform.UIKit.UIApplicationMain
+import platform.UIKit.UIColor
 import platform.UIKit.UIResponder
 import platform.UIKit.UIResponderMeta
 import platform.UIKit.UIScreen
 import platform.UIKit.UIWindow
+import platform.UIKit.backgroundColor
+import platform.UIKit.safeAreaInsets
+import platform.UIKit.safeAreaLayoutGuide
 import ru.alexgladkov.jetpackcomposedemo.ui.themes.JetHabitTheme
 import ru.alexgladkov.odyssey.compose.setup.OdysseyConfiguration
 import ru.alexgladkov.odyssey.compose.setup.setNavigationContent
@@ -57,36 +65,40 @@ class SkikoAppDelegate : UIResponder, UIApplicationDelegateProtocol {
     }
 
     override fun application(application: UIApplication, didFinishLaunchingWithOptions: Map<Any?, *>?): Boolean {
-        PlatformSDK.init(PlatformConfiguration())
         window = UIWindow(frame = UIScreen.mainScreen.bounds)
         window!!.rootViewController = Application("JetHabit") {
+            PlatformSDK.init(PlatformConfiguration())
             val settingsEventBus = remember { SettingsEventBus() }
             val currentSettings = settingsEventBus.currentSettings.collectAsState().value
 
-            Box(modifier = Modifier.fillMaxSize().background(Color.LightGray)) {
-                Text(modifier = Modifier.align(Alignment.Center), text = "Bounds ${UIScreen.mainScreen.bounds}")
-            }
+            MainTheme(
+                style = currentSettings.style,
+                darkTheme = currentSettings.isDarkMode,
+                corners = currentSettings.cornerStyle,
+                textSize = currentSettings.textSize,
+                paddingSize = currentSettings.paddingSize
+            ) {
+                val configuration = OdysseyConfiguration(
+                    backgroundColor = JetHabitTheme.colors.primaryBackground
+                )
 
-//            MainTheme(
-//                style = currentSettings.style,
-//                darkTheme = currentSettings.isDarkMode,
-//                corners = currentSettings.cornerStyle,
-//                textSize = currentSettings.textSize,
-//                paddingSize = currentSettings.paddingSize
-//            ) {
-//                val configuration = OdysseyConfiguration(
-//                    backgroundColor = JetHabitTheme.colors.primaryBackground
-//                )
-//
-//                CompositionLocalProvider(
-//                    LocalSettingsEventBus provides settingsEventBus
-//                ) {
-//                    setNavigationContent(configuration) {
-//                        navigationGraph()
-//                    }
-//                }
-//            }
+                CompositionLocalProvider(
+                    LocalSettingsEventBus provides settingsEventBus
+                ) {
+                    Column {
+                        Box(modifier = Modifier.fillMaxWidth().height(30.dp).background(JetHabitTheme.colors.primaryBackground))
+                        Box(modifier = Modifier.fillMaxWidth().weight(1f)) {
+                            setNavigationContent(configuration) {
+                                navigationGraph()
+                            }
+                        }
+                        Box(modifier = Modifier.fillMaxWidth().height(20.dp).background(JetHabitTheme.colors.primaryBackground))
+                    }
+                }
+            }
         }
+
+        window!!.backgroundColor = UIColor.whiteColor
         window!!.makeKeyAndVisible()
         return true
     }
