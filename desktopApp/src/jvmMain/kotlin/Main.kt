a9e86e77724f85107adf334d46175f21be7758ab
Alex <bob298@yandex.ru>
2023-08-09 06:32:53 +0800

    Remove annotation

--- desktopApp/src/jvmMain/kotlin/Main.kt
+++ desktopApp/src/jvmMain/kotlin/Main.kt
@@ -1,10 +1,8 @@
-import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.WindowState
 import androidx.compose.ui.window.singleWindowApplication
 
-@OptIn(ExperimentalComposeUiApi::class)
 fun main() =
     singleWindowApplication(
         title = "Jet Habit",
